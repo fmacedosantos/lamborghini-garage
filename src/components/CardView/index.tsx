7e8b8f7832b3b6837d408bf787ca87859e0d2ae0
fmacedosantos <felipemacedo986@gmail.com>
2024-08-20 12:30:20 -0300

    Começando o desenvolvimento do componente CardView

---
+++ src/components/CardView/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export default function CardView(){
+    return(
+        <View style={styles.imageContainer}>
+            {/**aula importando imagens */}
+            <Text>CardView Works!</Text>
+        </View>
+    )
+}
