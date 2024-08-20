f1e3e2490e2f7a993df0df73faf6726e8fd74ca9
fmacedosantos <felipemacedo986@gmail.com>
2024-08-20 11:01:20 -0300

    Aplicando os triângulos

--- src/screens/GarageScreen/index.tsx
+++ src/screens/GarageScreen/index.tsx
@@ -7,6 +7,9 @@ export function GarageScreen() {
     <View style={styles.container}>
       <View style={styles.cameraBox}>
         <View style={styles.triangleCorner}/>
+        <View style={[styles.triangleCorner, styles.triangleCornerTopRight]}/>
+        <View style={[styles.triangleCorner, styles.triangleCornerBottomRight]}/>
+        <View style={[styles.triangleCorner, styles.triangleCornerBottomLeft]}/>
       </View>
     </View>
   );
