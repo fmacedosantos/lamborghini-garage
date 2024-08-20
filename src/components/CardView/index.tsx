c09f95046e113d1c065056e35d2978996ff7a261
Felipe Macedo dos Santos <felipemacedo986@gmail.com>
2024-08-20 16:20:07 -0300

    Trabalhando com o logo e estilizando

--- src/components/CardView/index.tsx
+++ src/components/CardView/index.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { styles } from "./styles";
+
+import {Logo} from '../../../assets/pictures/logo.png'
 
 export default function CardView(){
     return(
         <View style={styles.imageContainer}>
-            {/**aula importando imagens */}
+            <View style={styles.logoContainer}>
+                <Image style={styles.imageLogo} source={Logo}/>
+            </View>
             <Text>CardView Works!</Text>
         </View>
     )
