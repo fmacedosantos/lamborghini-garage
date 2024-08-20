622c01b32356281bdc37b1f60dced703c2c9197c
fmacedosantos <felipemacedo986@gmail.com>
2024-08-20 11:01:06 -0300

    Criando os outros triângulos e retirando estilos desnecessários

--- src/screens/GarageScreen/styles.tsx
+++ src/screens/GarageScreen/styles.tsx
@@ -18,14 +18,25 @@ export const styles = StyleSheet.create({
     borderRadius: 10
   },
   triangleCorner: {
-    position: 'absolute',
-    width: 0,
-    height: 0,
-    borderStyle: 'solid',
+    position: 'absolute', // inicia na esquerda
     borderTopWidth: 20,
     borderTopColor: '#fff',
     borderRightWidth: 20,
     borderRightColor: 'transparent'
-
+  },
+  triangleCornerTopRight: {
+    top: 0,
+    right: 0,
+    transform: [{rotate: '90deg'}]
+  },
+  triangleCornerBottomRight: {
+    bottom: 0,
+    right: 0,
+    transform: [{rotate: '180deg'}]
+  },
+  triangleCornerBottomLeft: {
+    bottom: 0,
+    left: 0,
+    transform: [{rotate: '270deg'}]
   }
 });
