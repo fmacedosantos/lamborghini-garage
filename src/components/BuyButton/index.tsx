e35d83719a2f6e2500871e3a78fd4fae1928651a
Felipe Macedo dos Santos <felipemacedo986@gmail.com>
2024-08-20 19:19:47 -0300

    Criando e estilizando o BuyButton

---
+++ src/components/BuyButton/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+
+import { styles } from './styles';
+import {AntDesign} from '@expo/vector-icons'
+
+export default function BuyButton() {
+  return (
+    <View style={styles.container}>
+        <TouchableOpacity style={styles.button}>
+            <AntDesign name="shoppingcart" size={24} color="white"
+            style={styles.icon} />
+            <Text style={styles.buttonText}>Buy This</Text>
+        </TouchableOpacity>
+    </View>
+  );
+}
