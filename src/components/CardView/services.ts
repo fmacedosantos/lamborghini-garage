f1ac7bd22c9e69bf05ab66a5d6089dd09df8b2af
fmacedosantos <felipemacedo986@gmail.com>
2024-08-21 12:35:07 -0300

    Construindo a lógica do loadCarData

--- src/components/CardView/services.ts
+++ src/components/CardView/services.ts
@@ -1,5 +1,19 @@
-export const loadCarData = async() => {
+import { fetchCarData } from "../../api/getCars";
+import { CarModel } from "./model"
 
+export const loadCarData = async(
+    id: number,
+    setCarData: React.Dispatch<React.SetStateAction<CarModel | null>>
+) => {
+    const response = await fetchCarData(id)
+    try {
+        if(response){
+            setCarData(response)
+        }
+    } catch (error) {
+        console.log("Erro ao buscar da api", error);
+        setCarData(null)
+    }
 }
 
 export const handlePreviousItem = async() => {
