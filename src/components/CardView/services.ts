f87a552b44b2d5454871f5a7912bc4ea429608a4
Felipe Macedo dos Santos <89111798+fmacedosantos@users.noreply.github.com>
2024-08-21 16:20:17 -0300

    Criando os serviços de handleItems

--- src/components/CardView/services.ts
+++ src/components/CardView/services.ts
@@ -16,10 +16,38 @@ export const loadCarData = async(
     }
 }
 
-export const handlePreviousItem = async() => {
+export const handlePreviousItem = async(
+    carData: CarModel | null, 
+    setCarData: React.Dispatch<React.SetStateAction<CarModel | null>>
+    ) => {
+        let response = null;
+        try {
+            if(carData && carData?.id > 1){
+                response = await fetchCarData(carData.id - 1)
+            }
 
+            if(response){
+                setCarData(response)
+            }
+        } catch (error) {
+            console.log("Erro ao chamar a api", error);
+        }
 }
 
-export const handleNextItem = async() => {
-    
+export const handleNextItem = async(
+    carData: CarModel | null, 
+    setCarData: React.Dispatch<React.SetStateAction<CarModel | null>>
+    ) => {
+        let response = null
+    try {
+        if(carData && carData?.id < 10){
+            response = await fetchCarData(carData.id + 1)
+        }
+
+        if(response){
+            setCarData(response)
+        }
+    } catch (error) {
+        console.log("Erro ao chamar a api", error);
+    }
 }
